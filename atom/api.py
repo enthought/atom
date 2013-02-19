6817ab60abf713f0fded6e656c3b0c7787e0b406
S. Chris Colbert <sccolbert@gmail.com>
2013-02-19 00:24:38 -0500

    update the api file

--- atom/api.py
+++ atom/api.py
@@ -11,8 +11,44 @@ from .enum import Enum
 from .instance import Instance, ForwardInstance
 from .list import List
 from .scalars import (
-    Value, ReadOnly, Constant, Bool, Int, Long, Float, Str, Unicode, Callable
+    Value, ReadOnly, Constant, Bool, Int, Long, Float, Str, Unicode, Callable,
+    Range,
 )
-#from .tuple import Tuple
+from .tuple import Tuple
 from .typed import Typed, ForwardTyped
+from .typedevent import TypedEvent
+
+
+#: constant imports
+from .catom import (
+    NO_VALIDATE,
+    VALIDATE_READ_ONLY,
+    VALIDATE_CONSTANT,
+    VALIDATE_BOOL,
+    VALIDATE_INT,
+    VALIDATE_LONG,
+    VALIDATE_LONG_PROMOTE,
+    VALIDATE_FLOAT,
+    VALIDATE_FLOAT_PROMOTE,
+    VALIDATE_STR,
+    VALIDATE_UNICODE,
+    VALIDATE_UNICODE_PROMOTE,
+    VALIDATE_TUPLE,
+    VALIDATE_LIST,
+    VALIDATE_DICT,
+    VALIDATE_INSTANCE,
+    VALIDATE_TYPED,
+    VALIDATE_ENUM,
+    VALIDATE_CALLABLE,
+    VALIDATE_RANGE,
+    VALIDATE_OWNER_METHOD,
+    USER_VALIDATE,
+    NO_DEFAULT,
+    DEFAULT_VALUE,
+    DEFAULT_LIST,
+    DEFAULT_DICT,
+    DEFAULT_FACTORY,
+    DEFAULT_OWNER_METHOD,
+    USER_DEFAULT,
+)
 
